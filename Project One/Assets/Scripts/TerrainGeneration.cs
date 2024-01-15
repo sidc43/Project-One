36f3d03e3adb1a0f603b181b1f25e10c6ffb644b
Sidharth <imadethisjustfororiginbruh@gmail.com>
2024-01-15 16:44:59 -0600

    Added biomes and changed generation method
    A new biome can now be added by adding another element to the biomes array, giving it a color, settings the appropriate biome tiles, and then adding another handle to the biome gradient with the same color.

--- Project One/Assets/Scripts/TerrainGeneration.cs
+++ Project One/Assets/Scripts/TerrainGeneration.cs
@@ -5,11 +5,19 @@ using UnityEngine.Tilemaps;
 
 public class TerrainGeneration : MonoBehaviour
 {
+    [Header("Biome Classes")]
+    [SerializeField] private BiomeClass[] biomes;
+
     [Header("Generation Settings")]
     [SerializeField] private int worldWidth;
     [SerializeField] private int worldHeight;
     [SerializeField] private float noiseFreq, seed;
     [SerializeField] private Texture2D noiseTexture;
+
+    [Header("Biomes")]
+    [SerializeField] private float biomeFreq;
+    [SerializeField] private Texture2D biomeMap;
+    [SerializeField] private Gradient biomeGradient;
 
     [Header("Tilemap Settings")]
     [SerializeField] private Tilemap grassTileMap;
@@ -17,27 +25,53 @@ public class TerrainGeneration : MonoBehaviour
     [SerializeField] private Tilemap waterTileMap;
     [SerializeField] private Tile[] grass, sand, water;
 
+    private BiomeClass currentBiome;
+
+    private void OnValidate()
+    {
+        DrawTextures();
+    }
+
     void Start()
     {
         seed = Random.Range(-100000, 100000);
-        GenerateNoiseTexture();
+        DrawTextures();
         GenerateTerrain();
     }
-
+    private void DrawTextures()
+    {
+        GenerateNoiseTexture();
+        biomeMap = new Texture2D(worldWidth, worldHeight);
+        GenerateBiomeTexture();
+    }
     private void GenerateTerrain()
     {
         for (int x = 0; x < worldWidth; x++)
         {
             for (int y = 0; y < worldHeight; y++)
             {
-                if (noiseTexture.GetPixel(x, y).r < 0.5f)
-                    grassTileMap.SetTile(new Vector3Int(x, y), grass[Random.Range(0, grass.Length - 1)]);
-                else
-                    waterTileMap.SetTile(new Vector3Int(x, y), water[Random.Range(0, water.Length - 1)]);
+                currentBiome = GetCurrentBiome(x, y);
+                SetTileOfType(currentBiome.tileMap, new Vector3Int(x, y), currentBiome.groundTiles);
             }
         }
     }
-
+    private void SetTileOfType(Tilemap tilemap, Vector3Int pos, Tile[] tiles)
+    {
+        tilemap.SetTile(pos, tiles[Random.Range(0, tiles.Length - 1)]);
+    }
+    private void GenerateBiomeTexture()
+    {
+        for (int x = 0; x < biomeMap.width; x++)
+        {
+            for (int y = 0; y < biomeMap.height; y++)
+            {
+                float perlin = Mathf.PerlinNoise((x + seed) * biomeFreq, (y + seed) * biomeFreq);
+                Color c = biomeGradient.Evaluate(perlin);
+                biomeMap.SetPixel(x, y, c);
+            }
+        }
+        biomeMap.Apply();
+    }
     private void GenerateNoiseTexture()
     {
         noiseTexture = new Texture2D(worldWidth, worldHeight);
@@ -51,5 +85,19 @@ public class TerrainGeneration : MonoBehaviour
             }
         }
         noiseTexture.Apply();
+    }
+    private BiomeClass GetCurrentBiome(int x, int y)
+    {
+        // Change currentBiome
+        // Search through biomes
+        for (int i = 0; i < biomes.Length; i++)
+        {
+            if (biomes[i].biomeCol == biomeMap.GetPixel(x, y))
+            {
+                return biomes[i];
+            }
+        }
+
+        return currentBiome;
     }
 }

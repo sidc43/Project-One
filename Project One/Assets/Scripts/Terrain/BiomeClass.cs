f39f0e2c2f647875cf6d23f18e66a2419e45e442
Sidharth <imadethisjustfororiginbruh@gmail.com>
2024-01-16 16:32:24 -0600

    Finished biome generation and added player movement and animation

---
+++ Project One/Assets/Scripts/Terrain/BiomeClass.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[System.Serializable]
+public class BiomeClass
+{
+    [Header("General Settings")]
+    public string biomeName;
+    public Color biomeCol;
+
+    [Header("Generation Settings")]
+    public Tilemap tileMap;
+    public Tilemap treeTileMap;
+    public Tilemap foliageTileMap;
+    public Tilemap rockTileMap;
+    public Tile[] groundTiles, treeTiles, foliageTiles, rockTiles;
+
+    [Header("Foliage settings")]
+    [Range(0, 1)]
+    public float treeChance;
+    [Range(0, 1)]
+    public float foliageChance;
+    [Range(0, 1)]
+    public float rockChance;
+}
